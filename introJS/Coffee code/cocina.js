70baff8ca3dcc762a0152ee21aed793b8fb3ef0a
125053058-max <125053058@upq.edu.mx>
2026-09-23 19:25:20 -0600

    2da parte de coffee code, cocina 230926
    En esta parte se hicieron cambios en cocina de coffee code

--- introJS/Coffee code/cocina.js
+++ introJS/Coffee code/cocina.js
@@ -9,26 +9,57 @@ let menu = [
 function mostrar() {
     for (let i = 0; i < menu.length; i++) {
         console.log (menu[i].producto + ": $" + menu[i].precio);
+    
+    }
 
+    function agregarProducto(nombre, precio) {
+        menu.push({producto: nombre, precio});
+    }
+
+    function editaryBorrar() {
+        menu[1].precio = 75;
+        menu.splice(0,1);
+    
     }
 
     function productosBaratos() {
         let baratos = menu.filter (p => p.precio <=  50)
+        console.log("Productos baratos: ");
+        for (let i = 0; i < baratos.length; i++) {}
+        console.log(baratos[i].producto + " $" + baratos[i].precio);
+    }
+
+    function productosCaros() {
+        let caros = menu.filter (p => p.precio <= 80 )
+        console.log("Productos Caros: ");
+        for (let i = 0; i < caros.length; i++) {}
+        console.log(caros[i].producto + "$" + baratos[i].precio);
 
     }
-  
 
+    function buscarBebidas() {
+        let bebidas = menu.filter(p => p.tipo == "bebidas");
+        console.log("Bebidas:");
 
-}
+        for (let i = 0; i < bebidas.length; i++) {
+            console.log(bebidas[i].producto + ": $" + bebidas[i].precio);
+        }
+    }
 
-function editaryBorrar() {
-    menu[1].precio = 75;
-    menu.splice(0, 1);
+    function buscarPostres(){
+        let postres = menu.filter(p => p.tipo == "postres");
+        console.log("Postres:");
 
+        for (let i = 0; i < postres.length; i++) {}
+        console.log(postres[i].producto + ": $" + postres[i].precio);
+    }
 }
 
 mostrar();
-editaryBorrar();
-console.log("menu actualizado");
-mostrar();
-console.timeEnd("cafeteria");
+
+productosBaratos();
+prodtouctosCaros(); 
+buscarBebidas();
+buscarPostres();
+
+module.exports = {menu, mostrar, editaryBorrar, agregarProducto, productosBaratos, productosCaros, buscarBebidas, buscarPostres};
